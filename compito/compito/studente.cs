9f4af1e4a10b3c7b3f86643764fae4796597dc8f
gabriele castagnoli <gabriele castagnoli>
2017-01-15 10:58:20 +0100

    creazione classe studente

---
+++ compito/compito/studente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compito
+{
+    class studente : Persona
+    {
+        public string sezione {get ; set ;}
+        public Persona (string nome , string cognome , int anni) : base (nome , cognome , anni)
+    {
+        this.sezione = sezione;
+    }
+    }
+}
